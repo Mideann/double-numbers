2cb211a9e93a1b89f7b61f942b104dffc273c051
samuel <mideann@gmail.com>
2024-12-27 16:11:39 +0000

    double check

---
+++ main.py
@@ -0,0 +1,9 @@
+n=int(input('Enter a number'))
+if n>50:
+    print('Number is greater than 50')
+    if n%2==0 :
+        print('Number is even')
+    else:
+        print('Number is odd')
+else:
+    print('Number is less than 50')
